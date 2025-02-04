da9093b68a30100a1e61b64253f646f0e5068ece
jovillarrealm <javillarrm@eafit.edu.co>
2025-02-04 10:00:48 -0500

    add parameter error message

--- extract_gene_pipeline.sh
+++ extract_gene_pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+: "${1:?cnsg_script_dir is unset or null, pls give path to cnsg-downloader-code}"
+script_dir=$(dirname "$0")/
 cnsg_script_dir=$(realpath "$1")/
 date_format="%d-%m-%Y"
 today=$(date +"$date_format")
@@ -10,7 +11,7 @@ filters=${4:-"$gene,ssu,ssr"}
 feature_type=${5:-"rrna"}
 
 
-"$cnsg_script_dir"summary_download.sh -i "$taxon" -o "$taxon" -p GCF -l 10
-uv run filter.py "$taxon"/"$taxon"_"$today".tsv "$taxon"/"$taxon"_filtered_"$today".tsv
+"$cnsg_script_dir"summary_download.sh -i "$taxon" -o "$taxon" -p GCF ${6:+ -l "$6"}
+uv run "$script_dir"filter.py "$taxon"/"$taxon"_"$today".tsv "$taxon"/"$taxon"_filtered_"$today".tsv
 "$cnsg_script_dir"tsv_datasets_downloader.sh -i "$taxon"/"$taxon"_filtered_"$today".tsv -o "$taxon"/ --annotate=true
-uv run extract_gene.py "$taxon"/GENOMIC1/ "$taxon"/GFF1/ "$gene"/ "$gene" "$filters" "$feature_type"
+uv run "$script_dir"extract_gene.py "$taxon"/GENOMIC1/ "$taxon"/GFF1/ "$gene"/ "$gene" "$filters" "$feature_type"
